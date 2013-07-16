a53a1903a8b5d23e08baef2ac8c047d8fb7471e2
Dan Rathbone <dan.rathbone@socrata.com>
2013-07-16 14:49:00 -0700

    adding test annotations of bugs and exceptions

--- soda-fountain-lib/src/it/scala/com/socrata/soda/server/SoQLTypeIntegrationTest.scala
+++ soda-fountain-lib/src/it/scala/com/socrata/soda/server/SoQLTypeIntegrationTest.scala
@@ -44,9 +44,8 @@ class SoQLTypeIntegrationTest extends IntegrationTest with SoQLTypeIntegrationTe
     val uBody = JArray(Seq( JObject(row) ))
     val uResponse = dispatch("POST", "resource", Some(resourceName), None, None,  Some(uBody))
     assert(uResponse.getStatusCode == 200, uResponse.getResponseBody)
-
-    //Thread.sleep(6000)  //arg. wait out the race condition until we can guarantee consistency.
-
+    //TODO: remove the following line when ES can guarantee consistency.
+    Thread.sleep(3000)  //arg. wait out the race condition until we can guarantee consistency.
     waitForSecondaryStoreUpdate(resourceName, v)
     val params = Map(("$query" -> query))
     val qResponse = dispatch("GET", "resource", Some(resourceName), None, Some(params),  None)
@@ -56,15 +55,62 @@ class SoQLTypeIntegrationTest extends IntegrationTest with SoQLTypeIntegrationTe
 
   //test("upsert/query type json    ") { testType(Map(("test_id"->JNumber(110)), ("test_json    " -> )),  "select * where test_json    = ",  """[{test_json    :,  test_id:110.0 }]""".stripMargin) }
   test("upsert/query type number") { testType(Map(("test_id"->JNumber(100))),"select * where test_id = 100",  """[{test_id:100.0}]""".stripMargin)}
-  test("upsert/query type double") { testType(Map(("test_id"->JNumber(101)), ("test_double" -> JNumber(0.333))),  "select * where test_double = 0.333",  """[{test_double:0.333,  test_id:101.0 }]""".stripMargin) }
-  test("upsert/query type money") { testType(Map(("test_id"->JNumber(102)), ("test_money" -> JNumber(1.59))),  "select * where test_money = 1.59",  """[{test_money: 1.59,  test_id:102.0 }]""".stripMargin) }
+
+  test("upsert/query type double") {
+    /*
+     TODO:fix https://redmine.socrata.com/issues/11668
+     "errorCode":"query.soql.type-mismatch","data":{"data":{"function":"op$=","type":"*number","dataset":"alpha.551","position":{"row":1,"column":30,"line":"select * where test_double = 0.333}}}}
+     */
+    pendingUntilFixed{
+      testType(Map(("test_id"->JNumber(101)), ("test_double" -> JNumber(0.333))),  "select * where test_double = 0.333",  """[{test_double:0.333,  test_id:101.0 }]""".stripMargin)
+    }
+  }
+
+  test("upsert/query type money") {
+    /*
+    TODO: fix https://redmine.socrata.com/issues/11669
+    exception from soql-es-adapter: com.socrata.es.soql.NotImplementedException: Expression not implemented op$to_money(1.59 :: number) :: money
+     */
+    pendingUntilFixed{
+      testType(Map(("test_id"->JNumber(102)), ("test_money" -> JNumber(1.59))),  "select * where test_money = 1.59",  """[{test_money: 1.59,  test_id:102.0 }]""".stripMargin)
+    }
+  }
+
   test("upsert/query type text") { testType(Map(("test_id"->JNumber(103)), ("test_text" -> JString("eastlake"))),  "select * where test_text = 'eastlake'",  """[{test_id:103.0, test_text: 'eastlake'}]""".stripMargin) }
   test("upsert/query type object") { testType(Map(("test_id"->JNumber(107)), ("test_object" -> JObject(Map(("firstname"->JString("daniel")),("lastname"-> JString("rathbone")))))),  "select * where test_object.firstname::text = 'daniel'",  """[{test_object:{firstname:'daniel', lastname:'rathbone'},  test_id:107.0 }]""".stripMargin) }
-  test("upsert/query type array") { testType(Map(("test_id"->JNumber(108)), ("test_array" -> JArray(Seq(JBoolean(true), JNumber(99))))),  "select * where test_array[0]::boolean = true",  """[{test_array :[true, 99.0],  test_id:108.0 }]""".stripMargin) }
-  test("upsert/query type location") { testType(Map(("test_id"->JNumber(109)), ("test_location" -> JArray(Seq(JNumber(45.0), JNumber(39.0), JNull)))),  "select * where test_location.latitude = 45.0",  """[{test_location:[45.0, 39.0, null],  test_id:109.0 }]""".stripMargin) }
+
+  test("upsert/query type array") {
+    /*
+    TODO: fix https://redmine.socrata.com/issues/11670
+    com.socrata.es.soql.NotImplementedException: Expression not implemented cast$boolean(op$[](test_array :: array,0 :: number) :: json) :: boolean (query: select * where test_array0::boolean = true )
+     */
+    pendingUntilFixed{
+      testType(Map(("test_id"->JNumber(108)), ("test_array" -> JArray(Seq(JBoolean(true), JNumber(99))))),  "select * where test_array[0]::boolean = true",  """[{test_array :[true, 99.0],  test_id:108.0 }]""".stripMargin)
+    }
+  }
+
+  test("upsert/query type location") {
+    /* TODO: fix https://redmine.socrata.com/issues/11671 */
+    pendingUntilFixed{
+      testType(Map(("test_id"->JNumber(109)), ("test_location" -> JArray(Seq(JNumber(45.0), JNumber(39.0), JNull)))),  "select * where test_location.latitude = 45.0",  """[{test_location:[45.0, 39.0, null],  test_id:109.0 }]""".stripMargin)
+    }
+  }
+
   test("upsert/query type boolean") { testType(Map(("test_id"->JNumber(111)), ("test_boolean" -> JBoolean(true))),  "select * where test_boolean = true",  """[{test_id:111.0, test_boolean:true}]""".stripMargin) }
-  test("upsert/query type date") { testType(Map(("test_id"->JNumber(105)), ("test_date" -> JString("2013-07-15"))),  "select * where test_date    = '2013-07-15'",  """[{test_date    :'2013-07-15',  test_id:105.0 }]""".stripMargin) }
-  test("upsert/query type time") { testType(Map(("test_id"->JNumber(106)), ("test_time" -> JString("02:10:49.123"))),  "select * where test_time    = '02:10:49.123'",  """[{test_time    :'02:10:49.123',  test_id:106.0 }]""".stripMargin) }
+
+  test("upsert/query type date") {
+    //TODO: fix https://redmine.socrata.com/issues/11672
+    pendingUntilFixed{
+      testType(Map(("test_id"->JNumber(105)), ("test_date" -> JString("2013-07-15"))),  "select * where test_date    = '2013-07-15'",  """[{test_date    :'2013-07-15',  test_id:105.0 }]""".stripMargin)
+    }
+  }
+
+  test("upsert/query type time") {
+    //TODO: fix https://redmine.socrata.com/issues/11672
+    pendingUntilFixed{
+      testType(Map(("test_id"->JNumber(106)), ("test_time" -> JString("02:10:49.123"))),  "select * where test_time    = '02:10:49.123'",  """[{test_time    :'02:10:49.123',  test_id:106.0 }]""".stripMargin)
+    }
+  }
 
   test("upsert/query type fixed_timestamp   ") {
     testType(
