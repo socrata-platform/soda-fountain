f5497a89487831f003bbcb019a69c033bdfc74c8
Robert J. Macomber <robert.macomber@socrata.com>
2013-09-18 16:22:45 -0700

    c-json export format

--- soda-fountain-lib/src/main/scala/com/socrata/soda/server/resources/Export.scala
+++ soda-fountain-lib/src/main/scala/com/socrata/soda/server/resources/Export.scala
@@ -17,6 +17,8 @@ import javax.activation.MimeType
 import com.socrata.http.common.util.{AliasedCharset, ContentNegotiation}
 import com.socrata.http.server.implicits._
 import com.socrata.http.server.responses._
+import com.rojoma.json.codec.JsonCodec
+import com.socrata.soda.server.util.AdditionalJsonCodecs._
 
 case class Export(exportDAO: ExportDAO) {
   val log = org.slf4j.LoggerFactory.getLogger(classOf[Export])
@@ -24,7 +26,7 @@ case class Export(exportDAO: ExportDAO) {
   trait Exporter {
     val mimeType: MimeType
     val extension: Option[String]
-    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: Seq[ExportDAO.ColumnInfo], rows: Iterator[Array[SoQLValue]])
+    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: ExportDAO.CSchema, rows: Iterator[Array[SoQLValue]])
   }
 
   object JsonExporter extends Exporter {
@@ -32,8 +34,7 @@ case class Export(exportDAO: ExportDAO) {
     val mimeType = new MimeType(mimeTypeBase)
     val extension = Some("json")
 
-    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: Seq[ExportDAO.ColumnInfo], rows: Iterator[Array[SoQLValue]]) {
-      log.info("TODO: Negotiate charset")
+    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: ExportDAO.CSchema, rows: Iterator[Array[SoQLValue]]) {
       val mt = new MimeType(mimeTypeBase)
       mt.setParameter("charset", charset.alias)
       resp.setContentType(mt.toString)
@@ -44,8 +45,8 @@ case class Export(exportDAO: ExportDAO) {
         class Processor {
           val writer = w
           val jsonWriter = new CompactJsonWriter(writer)
-          val names: Array[String] = schema.map { ci => JString(ci.fieldName.name).toString }.toArray
-          val reps: Array[JsonColumnWriteRep] = schema.map { ci => JsonColumnRep.forClientType(ci.typ) }.toArray
+          val names: Array[String] = schema.schema.map { ci => JString(ci.fieldName.name).toString }.toArray
+          val reps: Array[JsonColumnWriteRep] = schema.schema.map { ci => JsonColumnRep.forClientType(ci.typ) }.toArray
 
           def writeJsonRow(row: Array[SoQLValue]) {
             writer.write('{')
@@ -81,13 +82,64 @@ case class Export(exportDAO: ExportDAO) {
     }
   }
 
+  object CJsonExporter extends Exporter {
+    val mimeTypeBase = "application/json+x-socrata-cjson"
+    val mimeType = new MimeType(mimeTypeBase)
+    val extension = Some("cjson")
+
+    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: ExportDAO.CSchema, rows: Iterator[Array[SoQLValue]]) {
+      val mt = new MimeType(mimeTypeBase)
+      mt.setParameter("charset", charset.alias)
+      resp.setContentType(mt.toString)
+      for {
+        rawWriter <- managed(resp.getWriter)
+        w <- managed(new BufferedWriter(rawWriter, 65536))
+      } yield {
+        val jw = new CompactJsonWriter(w)
+        val schemaOrdering = schema.schema.zipWithIndex.sortBy(_._1.fieldName).map(_._2).toArray
+        w.write(s"""[{"locale":${JString(schema.locale)}""")
+        w.write('\n')
+        schema.pk.foreach { pk =>
+          w.write(s""" ,"pk":${JString(pk.name)}""")
+          w.write('\n')
+        }
+        w.write(s""" ,"schema":[""")
+        var didOne = false
+        for(i <- 0 until schemaOrdering.length) {
+          if(didOne) w.write(',')
+          else didOne = true
+          val ci = schema.schema(schemaOrdering(i))
+          w.write(s"""{"c":${JString(ci.fieldName.name)},"t":${JsonCodec.toJValue(ci.typ)}}""")
+        }
+        w.write("]\n }\n")
+
+        // end of header
+
+        val reps: Array[JsonColumnWriteRep] = schema.schema.map { ci => JsonColumnRep.forClientType(ci.typ) }.toArray
+        for(row <- rows) {
+          w.write(",[")
+          if(row.length > 0) {
+            jw.write(reps(schemaOrdering(0)).toJValue(row(schemaOrdering(0))))
+            var i = 1
+            while(i < row.length) {
+              w.write(',')
+              jw.write(reps(schemaOrdering(i)).toJValue(row(schemaOrdering(i))))
+              i += 1
+            }
+          }
+          w.write("]\n")
+        }
+        w.write("]\n")
+      }
+    }
+  }
+
   object CsvExporter extends Exporter {
     val mimeTypeBase = "text/csv"
     val mimeType = new MimeType(mimeTypeBase)
     val extension = Some("csv")
 
-    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: Seq[ExportDAO.ColumnInfo], rows: Iterator[Array[SoQLValue]]) {
-      log.info("TODO: Negotiate charset")
+    def export(resp: HttpServletResponse, charset: AliasedCharset, schema: ExportDAO.CSchema, rows: Iterator[Array[SoQLValue]]) {
       val mt = new MimeType(mimeTypeBase)
       mt.setParameter("charset", charset.alias)
       resp.setContentType(mt.toString)
@@ -97,7 +149,7 @@ case class Export(exportDAO: ExportDAO) {
       } yield {
         class Processor {
           val writer = w
-          val reps: Array[CsvColumnWriteRep] = schema.map { f => CsvColumnRep.forType(f.typ) }.toArray
+          val reps: Array[CsvColumnWriteRep] = schema.schema.map { f => CsvColumnRep.forType(f.typ) }.toArray
           val sb = new java.lang.StringBuilder
 
           // somewhat surprisingly, writing cells into a stringbuilder and then
@@ -141,7 +193,7 @@ case class Export(exportDAO: ExportDAO) {
           }
 
           def go(rows: Iterator[Array[SoQLValue]]) {
-            val array = schema.map(_.humanName).toArray
+            val array = schema.schema.map(_.humanName).toArray
             writeCSVRow(array)
             while(rows.hasNext) {
               convertInto(array, rows.next())
@@ -155,7 +207,7 @@ case class Export(exportDAO: ExportDAO) {
     }
   }
 
-  val exporters = List(JsonExporter, CsvExporter)
+  val exporters = List(JsonExporter, CJsonExporter, CsvExporter)
 
   val exportForMimeType = exporters.map { e => e.mimeType -> e }.toMap
   val exporterExtensions = exporters.flatMap(_.extension).map(canonicalizeExtension).toSet
