e6adcd2fe78ca7f9173fa31735cf48e198ca823b
Robert J. Macomber <robert.macomber@socrata.com>
2013-09-25 14:36:11 -0700

    Build a soda-fountain assembly

--- project/SodaFountainJetty.scala
+++ project/SodaFountainJetty.scala
@@ -1,11 +1,23 @@
 import sbt._
 import Keys._
 
+import sbtassembly.Plugin.AssemblyKeys._
+import sbtassembly.Plugin.MergeStrategy
+import sbtassembly.AssemblyUtils._
+
 object SodaFountainJetty {
-  lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings() ++ Seq(
+  lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings(assembly = true) ++ Seq(
     libraryDependencies ++= Seq(
       "com.socrata" %% "socrata-http-jetty" % "2.0.0-SNAPSHOT",
       "com.socrata" %% "socrata-http-curator-broker" % "2.0.0-SNAPSHOT"
-    )
+    ),
+    mergeStrategy in assembly <<= (mergeStrategy in assembly) { old =>
+      {
+        case "about.html" => MergeStrategy.rename
+        case s if s.startsWith("scala/reflect/api/") =>
+          MergeStrategy.first // I hope this works...
+        case x => old(x)
+      }
+    }
   )
 }
