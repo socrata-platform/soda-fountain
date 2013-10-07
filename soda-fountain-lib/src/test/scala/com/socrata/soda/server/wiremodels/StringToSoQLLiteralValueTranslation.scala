3ef87440542b3b5198fc9e8f5dedb1735477428c
Robert J. Macomber <robert.macomber@socrata.com>
2013-10-07 11:28:37 -0700

    Update literal-generation tests
     * some more types can be generated as literals
     * others now have type annotations

--- soda-fountain-lib/src/test/scala/com/socrata/soda/server/wiremodels/StringToSoQLLiteralValueTranslation.scala
+++ soda-fountain-lib/src/test/scala/com/socrata/soda/server/wiremodels/StringToSoQLLiteralValueTranslation.scala
@@ -33,19 +33,19 @@ class StringToSoQLLiteralValueTranslation extends FunSuite with MustMatchers {
   // would we ever want to specify a null value as a row id?
   //test("JSON type checker handles nulls"){ testRep("null", "null", SoQLText) }
 
-  test("JSON rep type check with text"){ testRep("this is input", "\"this is input\"", SoQLText) }
+  test("JSON rep type check with text"){ testRep("this is input", "((\"this is input\")::text)", SoQLText) }
   test("JSON rep type check with unicode text"){
     val input = "this is unicode input text   صص صꕥꔚꔄꔞഝആ"
-    testRep(input, '"' + input +'"', SoQLText)
+    testRep(input, "((\"" + input + "\")::text)", SoQLText)
   }
-  test("JSON rep type check with number"){ testRep("12345", "12345", SoQLNumber) }
-  test("JSON rep type check with boolean"){ testRep("t", "true", SoQLBoolean) }
-  test("JSON rep type check with double"){ pendingUntilFixed { testRep("123.45", "123.45", SoQLDouble) } }
-  test("JSON rep type check with money"){ pendingUntilFixed { testRep("123.45", "123.45", SoQLMoney) } }
-  test("JSON rep type check with fixed timestamp"){ pendingUntilFixed { testRep("2013-06-03T02:26:05.123Z", "2013-06-03T02:26:05.123Z", SoQLFixedTimestamp) } }
-  test("JSON rep type check with floating timestamp"){ pendingUntilFixed { testRep("2013-06-03T02:26:05.123", "2013-06-03T02:26:05.123", SoQLFloatingTimestamp) } }
-  test("JSON rep type check with date"){ pendingUntilFixed { testRep("2013-06-03", "2013-06-03", SoQLDate) } }
-  test("JSON rep type check with time"){ pendingUntilFixed { testRep("02:26:05.123", "02:26:05.123", SoQLTime) } }
+  test("JSON rep type check with number"){ testRep("12345", "((12345)::number)", SoQLNumber) }
+  test("JSON rep type check with boolean"){ testRep("t", "((true)::boolean)", SoQLBoolean) }
+  test("JSON rep type check with double"){ pendingUntilFixed { testRep("123.45", "((123.45)::double)", SoQLDouble) } }
+  test("JSON rep type check with money"){ testRep("123.45", "((123.45)::money)", SoQLMoney) }
+  test("JSON rep type check with fixed timestamp"){ testRep("2013-06-03T02:26:05.123Z", "((\"2013-06-03T02:26:05.123Z\")::fixed_timestamp)", SoQLFixedTimestamp) }
+  test("JSON rep type check with floating timestamp"){ testRep("2013-06-03T02:26:05.123", "((\"2013-06-03T02:26:05.123\")::floating_timestamp)", SoQLFloatingTimestamp) }
+  test("JSON rep type check with date"){ pendingUntilFixed { testRep("2013-06-03", "((\"2013-06-03\")::date)", SoQLDate) } }
+  test("JSON rep type check with time"){ pendingUntilFixed { testRep("02:26:05.123", "((\"02:26:05.123\")::time)", SoQLTime) } }
   //test("JSON rep type check with object"){ testRep("{a:1}", "{a:1}", SoQLObject) }
   //test("JSON rep type check with array"){ testRep("[1,2,3]", "[1,2,3]", SoQLArray) }
 }
