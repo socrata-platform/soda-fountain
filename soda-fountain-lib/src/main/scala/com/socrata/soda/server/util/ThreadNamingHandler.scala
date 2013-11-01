93a0a3e7aac11d103948936c2c697e1d11006a61
Robert J. Macomber <robert.macomber@socrata.com>
2013-11-01 09:41:18 -0700

    Remove unused field
    ...it even had the incorrect value.

--- soda-fountain-lib/src/main/scala/com/socrata/soda/server/util/ThreadNamingHandler.scala
+++ soda-fountain-lib/src/main/scala/com/socrata/soda/server/util/ThreadNamingHandler.scala
@@ -4,8 +4,6 @@ import com.socrata.http.server.HttpService
 import javax.servlet.http.HttpServletRequest
 
 class ThreadNamingHandler(underlying: HttpService) extends HttpService {
-  val log = org.slf4j.LoggerFactory.getLogger(classOf[LoggingHandler])
-
   def apply(req: HttpServletRequest) = { resp =>
     val oldName = Thread.currentThread.getName
     try {
