5573728bc0864820153b633560d3ae9a1c7eed32
Andrew Burnell <andrew.burnell@socrata.com>
2014-03-25 09:41:11 -0700

    Temporarily force rojoma-json to 2.4.0 to build successfully

--- project/SodaFountainLibMacros.scala
+++ project/SodaFountainLibMacros.scala
@@ -4,7 +4,7 @@ import Keys._
 object SodaFountainLibMacros {
   lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings() ++ Seq(
     libraryDependencies ++= Seq(
-      "com.rojoma" %% "rojoma-json" % "[2.4.0,3.0.0)"
+      "com.rojoma" %% "rojoma-json" % "2.4.0"
     ),
     resolvers += Resolver.sonatypeRepo("snapshots"),
     addCompilerPlugin("org.scala-lang.plugins" % "macro-paradise_2.10.2" % "2.0.0-SNAPSHOT")
