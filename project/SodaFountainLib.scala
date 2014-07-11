941a5e1ccd34713074beee7b285e0213291b1e65
Evan Chan <velvia@gmail.com>
2014-07-11 15:48:54 -0700

    Upgrade to released version of thirdparty-utils

--- project/SodaFountainLib.scala
+++ project/SodaFountainLib.scala
@@ -13,7 +13,7 @@ object SodaFountainLib {
       "com.rojoma"         %% "simple-arm"                  % "[1.2.0,2.0.0)",
       "com.socrata"        %% "socrata-http-client"         % "[2.0.0,3.0.0)",
       "com.socrata"        %% "socrata-http-server"         % "[2.0.0,3.0.0)",
-      "com.socrata"        %% "socrata-thirdparty-utils"    % "2.3.1-SNAPSHOT",
+      "com.socrata"        %% "socrata-thirdparty-utils"    % "2.3.1",
       "com.socrata"        %% "soql-analyzer"               % "[0.2.0,1.0.0)",
       "com.socrata"        %% "soql-brita"                  % "[1.2.1,2.0.0)",
       "com.socrata"        %% "soql-standalone-parser"      % "[0.2.0,1.0.0)",
