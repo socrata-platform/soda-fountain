ba8ee38df7585e8333ac2d9d4e8fa350d5d46607
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2014-07-22 16:11:30 -0700

    Fix build break

--- soda-fountain-jetty/src/main/scala/com/socrata/soda/server/SodaFountainJetty.scala
+++ soda-fountain-jetty/src/main/scala/com/socrata/soda/server/SodaFountainJetty.scala
@@ -6,22 +6,26 @@ import com.socrata.soda.server.config.SodaFountainConfig
 import com.socrata.http.server.SocrataServerJetty
 import com.socrata.http.server.curator.CuratorBroker
 import org.apache.curator.x.discovery.ServiceDiscoveryBuilder
+import com.socrata.thirdparty.curator.DiscoveryFromConfig
+import com.socrata.http.common.AuxiliaryData
 
 object SodaFountainJetty extends App {
   val config = new SodaFountainConfig(ConfigFactory.load())
   for {
     sodaFountain <- managed(new SodaFountain(config))
-    discovery <- managed(ServiceDiscoveryBuilder.builder(classOf[Void]).
-      client(sodaFountain.curator).
-      basePath(config.curator.serviceBasePath).
-      build())
+    discovery <- managed(DiscoveryFromConfig.unmanaged(classOf[Void],
+                                                       sodaFountain.curator,
+                                                       config.discovery))
   } {
     discovery.start()
 
     val server = new SocrataServerJetty(
       sodaFountain.handle,
       port = config.network.port,
-      broker = new CuratorBroker[Void](discovery, config.serviceAdvertisement.address, config.serviceAdvertisement.service, None))
+      broker = new CuratorBroker[Void](discovery,
+                                       config.serviceAdvertisement.address,
+                                       config.serviceAdvertisement.service,
+                                       None))
 
     server.run()
   }
