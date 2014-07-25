d962523f61134cbb00f19e80d14e13550b9048dd
Evan Chan <velvia@gmail.com>
2014-07-25 13:30:28 -0700

    Upgrade to SBT 0.13 and newer cloudbees plugin

--- project/Build.scala
+++ project/Build.scala
@@ -4,13 +4,8 @@ object Build extends sbt.Build {
   lazy val build = Project(
     "soda-fountain",
     file(".")
-  ).settings(BuildSettings.buildSettings : _*).aggregate(allOtherProjects: _*)
-
-  private def allOtherProjects =
-    for {
-      method <- getClass.getDeclaredMethods.toSeq
-      if method.getParameterTypes.isEmpty && classOf[Project].isAssignableFrom(method.getReturnType) && method.getName != "build"
-    } yield method.invoke(this).asInstanceOf[Project] : ProjectReference
+  ).settings(BuildSettings.buildSettings : _*).aggregate(sodaFountainLib, sodaFountainJetty,
+                                                         sodaFountainWar, sodaFountainLibMacros)
 
   private def p(name: String, settings: { def settings: Seq[Setting[_]] }, dependencies: ClasspathDep[ProjectReference]*) =
     Project(name, file(name)).settings(settings.settings : _*).configs(IntegrationTest).dependsOn(dependencies: _*)
