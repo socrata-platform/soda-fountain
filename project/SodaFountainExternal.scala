08f0ea4a5921d9d4e6233a0557657677b193235a
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2014-10-27 09:29:47 -0700

    Bump minor version. Align thirdparty-utils and thirdparty-test-utils version.

--- project/SodaFountainExternal.scala
+++ project/SodaFountainExternal.scala
@@ -9,17 +9,17 @@ object SodaFountainExternal {
   lazy val settings: Seq[Setting[_]] = BuildSettings.projectSettings() ++
     Seq(
       libraryDependencies ++= Seq(
-        "com.rojoma"               %% "rojoma-json"              % "2.4.3",
-        "com.rojoma"               %% "simple-arm"               % "1.2.0",
-        "com.socrata"              %% "socrata-http-client"      % "2.0.0",
-        "com.socrata"              %% "socrata-thirdparty-utils" % "2.4.0",
-        "com.typesafe"              % "config"                   % "1.0.2",
-        "org.apache.curator"        % "curator-x-discovery"      % "2.4.2"
+        "com.rojoma"               %% "rojoma-json"                   % "2.4.3",
+        "com.rojoma"               %% "simple-arm"                    % "1.2.0",
+        "com.socrata"              %% "socrata-http-client"           % "2.0.0",
+        "com.socrata"              %% "socrata-thirdparty-utils"      % "2.5.0",
+        "com.typesafe"              % "config"                        % "1.0.2",
+        "org.apache.curator"        % "curator-x-discovery"           % "2.4.2"
           exclude("org.slf4j", "slf4j-log4j12")
           exclude("log4j", "log4j"),
-        "com.github.tomakehurst"    % "wiremock"                 % "1.46"    % "test",
-        "com.socrata"        %% "socrata-thirdparty-test-utils" % "2.5.0" % "test",
-        "org.apache.curator"        % "curator-test"             % "2.4.2"   % "test",
-        "org.scalatest"            %% "scalatest"                % "2.2.0"   % "test"
+        "com.github.tomakehurst"    % "wiremock"                      % "1.46"    % "test",
+        "com.socrata"              %% "socrata-thirdparty-test-utils" % "2.5.0"   % "test",
+        "org.apache.curator"        % "curator-test"                  % "2.4.2"   % "test",
+        "org.scalatest"            %% "scalatest"                     % "2.2.0"   % "test"
       ))
 }
