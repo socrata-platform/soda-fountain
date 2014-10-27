bb0cd9bbd2e8a9ce025ad79efd0b9cd7db0f71e5
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2014-10-27 09:37:45 -0700

    Use the latest non-snapshot version of third party utils

--- project/SodaFountainExternal.scala
+++ project/SodaFountainExternal.scala
@@ -12,13 +12,13 @@ object SodaFountainExternal {
         "com.rojoma"               %% "rojoma-json"                   % "2.4.3",
         "com.rojoma"               %% "simple-arm"                    % "1.2.0",
         "com.socrata"              %% "socrata-http-client"           % "2.0.0",
-        "com.socrata"              %% "socrata-thirdparty-utils"      % "2.5.0",
+        "com.socrata"              %% "socrata-thirdparty-utils"      % "2.5.2",
         "com.typesafe"              % "config"                        % "1.0.2",
         "org.apache.curator"        % "curator-x-discovery"           % "2.4.2"
           exclude("org.slf4j", "slf4j-log4j12")
           exclude("log4j", "log4j"),
         "com.github.tomakehurst"    % "wiremock"                      % "1.46"    % "test",
-        "com.socrata"              %% "socrata-thirdparty-test-utils" % "2.5.0"   % "test",
+        "com.socrata"              %% "socrata-thirdparty-test-utils" % "2.5.2"   % "test",
         "org.apache.curator"        % "curator-test"                  % "2.4.2"   % "test",
         "org.scalatest"            %% "scalatest"                     % "2.2.0"   % "test"
       ))
