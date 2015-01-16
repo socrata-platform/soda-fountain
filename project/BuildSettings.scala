18cb22911f69071976e3595185bec724aa69157e
Evan Chan <evan.chan@socrata.com>
2015-01-16 10:09:57 -0800

    Add codacy code coverage; update to SCoverage 1.0.1

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -2,6 +2,7 @@ import sbt._
 import Keys._
 
 import com.socrata.cloudbeessbt.SocrataCloudbeesSbt
+import scoverage.ScoverageSbtPlugin
 
 object BuildSettings {
   val buildSettings: Seq[Setting[_]] =
@@ -9,7 +10,8 @@ object BuildSettings {
     spray.revolver.RevolverPlugin.Revolver.settings ++
     Defaults.itSettings ++
       Seq(
-        scalaVersion := "2.10.4"
+        scalaVersion := "2.10.4",
+        ScoverageSbtPlugin.ScoverageKeys.coverageHighlighting := false
       )
 
   def projectSettings(assembly: Boolean = false): Seq[Setting[_]] =
