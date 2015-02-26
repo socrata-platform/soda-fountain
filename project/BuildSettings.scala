7ef284579f41cfe398143919a8e5b7b9a1fdb04a
John Krah <john.krah@socrata.com>
2015-02-26 12:02:14 -0800

    temporarily disable scalastyle checks

--- project/BuildSettings.scala
+++ project/BuildSettings.scala
@@ -1,13 +1,15 @@
+import com.socrata.sbtplugins.StylePlugin.StyleKeys._
+import sbt.Keys._
 import sbt._
-import Keys._
-
-import scoverage.ScoverageSbtPlugin
 
 object BuildSettings {
-  val buildSettings: Seq[Setting[_]] =
+  def buildSettings: Seq[Setting[_]] =
     spray.revolver.RevolverPlugin.Revolver.settings ++
     Defaults.itSettings ++
       Seq(
+        // TODO: enable style checks
+        styleCheck in Test := {},
+        styleCheck in Compile := {},
         scalaVersion := "2.10.4"
       )
 
