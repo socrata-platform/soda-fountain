3951e33455babdde1d6da57eff5c14f605876c9a
Evan Chan <evan.chan@socrata.com>
2015-03-26 16:53:18 -0700

    Update start script to point at docs repo

--- bin/start_soda_fountain.sh
+++ bin/start_soda_fountain.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Start Soda Fountain main server
 BASEDIR=$(dirname $0)/..
-CONFIG=${SODA_CONFIG:-$BASEDIR/../onramp/services/soda2.conf}
+CONFIG=${SODA_CONFIG:-$BASEDIR/../docs/onramp/services/soda2.conf}
 JARFILE=$BASEDIR/soda-fountain-jetty/target/scala-2.10/soda-fountain-jetty-assembly-*.jar
 if [ ! -e $JARFILE ]; then
   cd $BASEDIR && sbt assembly
