53d9ad7ed4464c0dd78afbf524b3751b663daeba
Urmila Nadkarni <urmila.nadkarni@socrata.com>
2015-04-27 17:12:05 -0700

    Log healthz errors in the right place

--- soda-fountain-lib/src/main/scala/com/socrata/soda/clients/geospace/GeospaceClient.scala
+++ soda-fountain-lib/src/main/scala/com/socrata/soda/clients/geospace/GeospaceClient.scala
@@ -28,41 +28,29 @@ trait GeospaceClient {
 case class CuratedGeospaceClient[T](discovery: ServiceDiscovery[T], config: GeospaceClientConfig)
   extends CuratorServiceBase(discovery, config.serviceName) with GeospaceClient {
 
-  val logger = LoggerFactory.getLogger(getClass)
-
   private def urlPrefix: Option[String] = Option(provider.getInstance()).map { serv => serv.buildUriSpec() }
 
   private def request(url: String): Http.Request = Http.get(url)
     .option(HttpOptions.connTimeout(config.connectTimeout.toMillis.toInt))
     .option(HttpOptions.readTimeout(config.readTimeout.toMillis.toInt))
 
-  private def logVersionError(url: String, status: Int, response: String): Unit =
-    logger.error(s"Unexpected response from Geospace @ $url: " +
-      s"status $status, " +
-      s"response: ${response.take(PartialResponseLength)}")
-
   def versionCheck = {
     urlPrefix match {
       case Some(prefix) =>
         val url = prefix + "version"
-
         try {
           val (status, _, response) = request(url).asHeadersAndParse(Http.readString)
           if (status == 200 && response.contains("version")) {
             Success
           } else {
-            logVersionError(url, status, response)
             Failure(url, status, response.take(PartialResponseLength))
           }
         } catch {
           case e: scalaj.http.HttpException =>
-            logVersionError(url, e.code, e.body)
             Failure(url, e.code, e.body.take(PartialResponseLength))
         }
       case None =>
-        val msg = "Unable to get geospace instance from Curator"
-        logger.error(msg)
-        Failure("", 0, msg)
+        Failure("", 0, "Unable to get geospace instance from Curator")
     }
   }
 }
--- soda-fountain-lib/src/main/scala/com/socrata/soda/server/resources/HealthZ.scala
+++ soda-fountain-lib/src/main/scala/com/socrata/soda/server/resources/HealthZ.scala
@@ -10,12 +10,18 @@ import com.rojoma.json.v3.codec.JsonEncode
 case class HealthZ(geospace: GeospaceClient) {
   val log = org.slf4j.LoggerFactory.getLogger(getClass)
 
+  private def logVersionError(url: String, status: Int, response: String): Unit =
+    log.error(s"Unexpected response from Geospace @ $url: " +
+      s"status $status, " +
+      s"response: ${response.take(PartialResponseLength)}")
+
   object service extends SodaResource {
     override def get = { req => response =>
       val (code, fields) = geospace.versionCheck match {
         case Success =>
           (OK, Map("geospace_ok" -> JBoolean.canonicalTrue))
         case f: Failure =>
+          logVersionError(f.url, f.status, f.partialResponse)
           val details = JsonEncode.toJValue(f)
           (InternalServerError, Map("geospace_ok" -> JBoolean.canonicalFalse, "details" -> details))
       }
