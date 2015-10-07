713a57f768583a6ccb6231da0abbcd129adeb3a4
Kaida Masaki <kaida.masaki@socrata.com>
2015-10-07 13:58:46 -0700

    Handle multiple jars

--- bin/run_migrations.sh
+++ bin/run_migrations.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 # Run soda fountain migrations.
 # See README file for options - but command can be migrate / undo / redo
-BASEDIR=$(dirname $0)/..
+BASEDIR=$(dirname "$0")/..
 if [ -z "$SODA_CONFIG" ]; then
     if [ -e "$BASEDIR/../docs/onramp/services/soda2.conf" ]; then
         echo "Using soda2.conf from docs directory"
@@ -13,10 +13,12 @@ else
     echo "Using soda2.conf specified in $SODA_CONFIG environment variable"
     CONFIG=$SODA_CONFIG
 fi
-JARFILE=$BASEDIR/soda-fountain-jetty/target/scala-2.10/soda-fountain-jetty-assembly-*.jar
-if [ ! -e $JARFILE ]; then
-  cd $BASEDIR && sbt assembly
+JARS=( $BASEDIR/soda-fountain-jetty/target/scala-2.10/soda-fountain-jetty-assembly-*.jar )
+# shellcheck disable=SC2012
+JARFILE=$(ls -t "${JARS[@]}" | head -n 1)
+if [ ! -e "$JARFILE" ]; then
+  cd "$BASEDIR" && sbt assembly
 fi
 COMMAND=${1:-migrate}
-echo Running soda.server.MigrateSchema $COMMAND $2...
-java -Dconfig.file=$CONFIG -cp $JARFILE com.socrata.soda.server.MigrateSchema $COMMAND $2
+echo Running soda.server.MigrateSchema "$COMMAND" "$2"...
+java -Dconfig.file="$CONFIG" -cp "$JARFILE" com.socrata.soda.server.MigrateSchema "$COMMAND" "$2"
