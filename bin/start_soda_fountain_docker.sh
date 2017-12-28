b319f7b352c31b8299d2928fc81a5f9691fd433e
Michael Brown <michael.brown@socrata.com>
2017-12-27 16:20:33 -0800

    remove env variables from run script

--- bin/start_soda_fountain_docker.sh
+++ bin/start_soda_fountain_docker.sh
@@ -9,16 +9,6 @@ fi
 
 AWS_PROFILE=infrastructure docker run \
            -p 6010:6010 \
-           -e ZOOKEEPER_ENSEMBLE="[ \"local.dev.socrata.net\" ]" \
-           -e ARK_HOST="local.dev.socrata.net" \
-           -e SODA_FOUNTAIN_DB_HOST="local.dev.socrata.net" \
-           -e SODA_FOUNTAIN_DB_PORT=5432 \
-           -e SODA_FOUNTAIN_DB_NAME="sodafountain" \
-           -e SODA_FOUNTAIN_DB_USER="blist" \
-           -e SODA_FOUNTAIN_DB_PASSWORD_LINE="password = \"blist\"" \
-           -e DATA_COORDINATORS_FOR_NEW_DATASETS="primus" \
-           -e SPANDEX_HOST="local.dev.socrata.net" \
-           -e SPANDEX_PORT=8042 \
-           -t "$image"
+           -d -t "$image"
 
 
