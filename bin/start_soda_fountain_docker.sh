decad2140252c7631fc00c8ccb8159fa4e947d78
Michael Brown <michael.brown@socrata.com>
2017-12-27 17:02:28 -0800

    print error if docker tag is not supplied

--- bin/start_soda_fountain_docker.sh
+++ bin/start_soda_fountain_docker.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
 image_tag="$1"
-if [ "$image_tag" == "local" ]; then
+if [ -z "$image_tag" ]; then
+  echo "ERR>> must specify an image tag or `local` if you want to build your own container"
+  exit 1
+elif [ "$image_tag" == "local" ]; then
   image="soda-fountain"
 else
   image="649617362025.dkr.ecr.us-west-2.amazonaws.com/internal/soda-fountain:$image_tag"
