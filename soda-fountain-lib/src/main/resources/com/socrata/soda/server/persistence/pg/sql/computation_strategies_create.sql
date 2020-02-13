44f18c170b05f4b7131d1ce19efacfe3f31e1643
Marc Slemko <marc.slemko@socrata.com>
2020-02-13 10:57:12 -0800

    Make tests compatible with postgres 12.0+.
    Explicitly name the foreign key for backwards compatibility as
    postgres 12.0 changed the default name for foreign keys to include
    all columns.

--- soda-fountain-lib/src/main/resources/com/socrata/soda/server/persistence/pg/sql/computation_strategies_create.sql
+++ soda-fountain-lib/src/main/resources/com/socrata/soda/server/persistence/pg/sql/computation_strategies_create.sql
@@ -5,6 +5,8 @@ CREATE TABLE IF NOT EXISTS computation_strategies (
   recompute                             BOOLEAN NOT NULL,
   source_columns                        TEXT[],
   parameters                            TEXT,
-  UNIQUE (dataset_system_id, column_id),
-  FOREIGN KEY (dataset_system_id, column_id) REFERENCES columns (dataset_system_id, column_id)
+  UNIQUE (dataset_system_id, column_id)
 );
+
+-- Explicitly name the foreign key for backwards compatibility as postgres 12.0 changed the default name for foreign keys to include all columns
+ALTER TABLE computation_strategies ADD CONSTRAINT computation_strategies_dataset_system_id_fkey FOREIGN KEY (dataset_system_id, column_id) REFERENCES columns(dataset_system_id, column_id);
