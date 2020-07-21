788f27257056f84e63aa4f3a88e19ccde845b501
Robert J. Macomber <robert.macomber@socrata.com>
2020-07-21 15:47:16 -0700

    The ThreadLimiter is just a semaphore; make that explicit

--- soda-fountain-lib/src/main/scala/com/socrata/soda/server/ThreadLimiter.scala
+++ soda-fountain-lib/src/main/scala/com/socrata/soda/server/ThreadLimiter.scala
@@ -1,21 +1,23 @@
 package com.socrata.soda.server
 
-import java.util.concurrent.atomic.AtomicInteger
+import java.util.concurrent.Semaphore
 
 import org.slf4j.LoggerFactory
 
 final class ThreadLimiter(consumerName: String, maxThreads: Int) {
   private val log = LoggerFactory.getLogger(classOf[ThreadLimiter])
 
-  val usedThreads: AtomicInteger = new AtomicInteger(0)
+  private val semaphore = new Semaphore(maxThreads)
 
   def withThreadpool[T](f: => T): T = {
-    try {
-      if (usedThreads.incrementAndGet() > maxThreads)
-        throw TooManyThreadsException(consumerName)
-      f
-    } finally {
-      usedThreads.decrementAndGet()
+    if(semaphore.tryAcquire()) {
+      try {
+        f
+      } finally {
+        semaphore.release()
+      }
+    } else {
+      throw TooManyThreadsException(consumerName)
     }
   }
 }
