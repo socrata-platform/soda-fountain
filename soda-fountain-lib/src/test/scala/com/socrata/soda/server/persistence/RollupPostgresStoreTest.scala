f641b677d58e5bae1fb5d7010f691af8201c99d7
Paul Maksimovich <paul.maksimovich@tylertech.com>
2023-04-06 10:36:39 -0400

    rollup last accessed tests

--- soda-fountain-lib/src/test/scala/com/socrata/soda/server/persistence/RollupPostgresStoreTest.scala
+++ soda-fountain-lib/src/test/scala/com/socrata/soda/server/persistence/RollupPostgresStoreTest.scala
@@ -1,9 +1,9 @@
 package com.socrata.soda.server.persistence
 
-import com.socrata.soda.clients.datacoordinator.DataCoordinatorClient.{Result => DcResult}
-import com.socrata.soda.clients.datacoordinator.{DataCoordinatorClient, DataCoordinatorInstruction}
+import com.socrata.soda.clients.datacoordinator.DataCoordinatorClient.{RollupResult, Result => DcResult}
+import com.socrata.soda.clients.datacoordinator.{DataCoordinatorClient, DataCoordinatorInstruction, RollupInfo}
 import com.socrata.soda.server.highlevel.DatasetDAO
-import com.socrata.soda.server.highlevel.DatasetDAO.{RollupMarkedAccessed, Result => DdResult}
+import com.socrata.soda.server.highlevel.DatasetDAO.{RollupMarkedAccessed, Rollups, Result => DdResult}
 import com.socrata.soda.server.id.{DatasetHandle, RollupName}
 import com.socrata.soda.server.wiremodels.UserProvidedRollupSpec
 import org.joda.time.DateTime
@@ -12,65 +12,167 @@ import org.scalatest.matchers.should.Matchers
 class RollupPostgresStoreTest extends SodaPostgresContainerTest with Matchers {
 
 
-  describe("Rollups"){
-    describe("that do not exist"){
-      it("should not be marked as accessed") {
-        val nameAndSchemaStore: NameAndSchemaStore = buildNameAndSchemaStore()
+  describe("Rollups") {
 
-        val datasetDao: DatasetDAO = buildDatasetDao(
-          nameAndSchemaStore = nameAndSchemaStore
-        )
+    it("should not be marked as accessed") {
+      val nameAndSchemaStore: NameAndSchemaStore = buildNameAndSchemaStore()
 
-        val dataset = generateDataset("one1234", Seq.empty[ColumnRecord])
-        nameAndSchemaStore.addResource(dataset)
+      val datasetDao: DatasetDAO = buildDatasetDao(
+        nameAndSchemaStore = nameAndSchemaStore
+      )
 
-        datasetDao.markRollupAccessed(dataset.resourceName, new RollupName("one")) match {
-          case RollupMarkedAccessed() => fail("Should have failed to mark rollup as accessed")
-          case _ =>
-        }
-      }
-    }
-    describe("that exist"){
-      it("should be marked as accessed") {
-        val nameAndSchemaStore: NameAndSchemaStore = buildNameAndSchemaStore()
-        val dcClient: DataCoordinatorClient = mock[DataCoordinatorClient]
-        (dcClient.update(_: DatasetHandle, _: String, _: Option[Long], _: String, _: Iterator[DataCoordinatorInstruction])(_: DcResult => DdResult)).expects(
-          *, *, *, *, *, *
-        ).onCall((_, _, _, _, _, r) => r(DataCoordinatorClient.NonCreateScriptResult(
-          null,
-          None,
-          1,
-          0,
-          0,
-          DateTime.now()
-        )))
+      val dataset = generateDataset("one1234", Seq.empty[ColumnRecord])
+      nameAndSchemaStore.addResource(dataset)
 
-        val datasetDao: DatasetDAO = buildDatasetDao(
-          nameAndSchemaStore = nameAndSchemaStore,
-          dataCoordinatorClient = dcClient
-        )
-
-        val dataset = generateDataset("one1234", Seq.empty[ColumnRecord])
-        nameAndSchemaStore.addResource(dataset)
-
-        val x = datasetDao.replaceOrCreateRollup(
-          "user1",
-          dataset.resourceName,
-          None,
-          new RollupName("one"),
-          UserProvidedRollupSpec(
-            Some(new RollupName("one")),
-            Some("select *")
-          )
-        )
-
-        datasetDao.markRollupAccessed(dataset.resourceName, new RollupName("one")) match {
-          case RollupMarkedAccessed() =>
-          case _ => fail("Should have succeeded to mark rollup as accessed")
-        }
+      datasetDao.markRollupAccessed(dataset.resourceName, new RollupName("one")) match {
+        case RollupMarkedAccessed() => fail("Should have failed to mark rollup as accessed")
+        case _ =>
       }
     }
 
+    it("should be marked as accessed") {
+      val nameAndSchemaStore: NameAndSchemaStore = buildNameAndSchemaStore()
+      val dcClient: DataCoordinatorClient = mock[DataCoordinatorClient]
+      (dcClient.update(_: DatasetHandle, _: String, _: Option[Long], _: String, _: Iterator[DataCoordinatorInstruction])(_: DcResult => DdResult)).expects(
+        *, *, *, *, *, *
+      ).onCall((_, _, _, _, _, r) => r(DataCoordinatorClient.NonCreateScriptResult(
+        null,
+        None,
+        1,
+        0,
+        0,
+        DateTime.now()
+      )))
+
+      val datasetDao: DatasetDAO = buildDatasetDao(
+        nameAndSchemaStore = nameAndSchemaStore,
+        dataCoordinatorClient = dcClient
+      )
+
+      val dataset = generateDataset("one1234", Seq.empty[ColumnRecord])
+      nameAndSchemaStore.addResource(dataset)
+
+      val x = datasetDao.replaceOrCreateRollup(
+        "user1",
+        dataset.resourceName,
+        None,
+        new RollupName("one"),
+        UserProvidedRollupSpec(
+          Some(new RollupName("one")),
+          Some("select *")
+        )
+      )
+
+      datasetDao.markRollupAccessed(dataset.resourceName, new RollupName("one")) match {
+        case RollupMarkedAccessed() =>
+        case _ => fail("Should have succeeded to mark rollup as accessed")
+      }
+    }
+
+    it("should show as accessed") {
+      val nameAndSchemaStore: NameAndSchemaStore = buildNameAndSchemaStore()
+      val dcClient: DataCoordinatorClient = mock[DataCoordinatorClient]
+      (dcClient.update(_: DatasetHandle, _: String, _: Option[Long], _: String, _: Iterator[DataCoordinatorInstruction])(_: DcResult => DdResult)).expects(
+        *, *, *, *, *, *
+      ).onCall((_, _, _, _, _, r) => r(DataCoordinatorClient.NonCreateScriptResult(
+        null,
+        None,
+        1,
+        0,
+        0,
+        DateTime.now()
+      )))
+
+      val datasetDao: DatasetDAO = buildDatasetDao(
+        nameAndSchemaStore = nameAndSchemaStore,
+        dataCoordinatorClient = dcClient
+      )
+
+      val dataset = generateDataset("one1234", Seq.empty[ColumnRecord])
+      nameAndSchemaStore.addResource(dataset)
+
+      val x = datasetDao.replaceOrCreateRollup(
+        "user1",
+        dataset.resourceName,
+        None,
+        new RollupName("one"),
+        UserProvidedRollupSpec(
+          Some(new RollupName("one")),
+          Some("select *")
+        )
+      )
+
+      datasetDao.markRollupAccessed(dataset.resourceName, new RollupName("one")) match {
+        case RollupMarkedAccessed() =>
+        case _ => fail("Should have succeeded to mark rollup as accessed")
+      }
+
+      (dcClient.getRollups(_:DatasetHandle)).expects(
+        *
+      ).returning(
+        RollupResult(Seq.empty)
+      )
+
+      datasetDao.getRollups(dataset.resourceName) match{
+        case Rollups(spec) => spec.filter(_.lastAccessed.isDefined) should have size(1)
+        case _ => fail("Rollups should have been present")
+      }
+    }
+
+    it("should show as accessed, with dc") {
+      val nameAndSchemaStore: NameAndSchemaStore = buildNameAndSchemaStore()
+      val dcClient: DataCoordinatorClient = mock[DataCoordinatorClient]
+      (dcClient.update(_: DatasetHandle, _: String, _: Option[Long], _: String, _: Iterator[DataCoordinatorInstruction])(_: DcResult => DdResult)).expects(
+        *, *, *, *, *, *
+      ).onCall((_, _, _, _, _, r) => r(DataCoordinatorClient.NonCreateScriptResult(
+        null,
+        None,
+        1,
+        0,
+        0,
+        DateTime.now()
+      )))
+
+      val datasetDao: DatasetDAO = buildDatasetDao(
+        nameAndSchemaStore = nameAndSchemaStore,
+        dataCoordinatorClient = dcClient
+      )
+
+      val dataset = generateDataset("one1234", Seq.empty[ColumnRecord])
+      nameAndSchemaStore.addResource(dataset)
+
+      val x = datasetDao.replaceOrCreateRollup(
+        "user1",
+        dataset.resourceName,
+        None,
+        new RollupName("one"),
+        UserProvidedRollupSpec(
+          Some(new RollupName("one")),
+          Some("select *")
+        )
+      )
+
+      datasetDao.markRollupAccessed(dataset.resourceName, new RollupName("one")) match {
+        case RollupMarkedAccessed() =>
+        case _ => fail("Should have succeeded to mark rollup as accessed")
+      }
+
+      (dcClient.getRollups(_: DatasetHandle)).expects(
+        *
+      ).returning(
+        RollupResult(List(
+          RollupInfo(new RollupName("two"),"select 8")
+        ))
+      )
+
+      datasetDao.getRollups(dataset.resourceName) match {
+        case Rollups(spec) => {
+          spec should have size(2)
+          spec.filter(_.lastAccessed.isDefined) should have size (1)
+        }
+        case _ => fail("Rollups should have been present")
+      }
+    }
   }
 
 }
