81c326c8fe70c69549f70fbb47b350af29ffb352
Robert J. Macomber <robert.macomber@socrata.com>
2023-08-22 10:18:10 -0700

    Update bin scripts to reflect the new simplified assembly naming

--- bin/build.sh
+++ bin/build.sh
@@ -5,14 +5,13 @@ REALPATH=$(python -c "import os; print(os.path.realpath('$0'))")
 BASEDIR="$(dirname "${REALPATH}")/.."
 
 cd "$BASEDIR"
-JARFILE="$(ls -rt soda-fountain-jetty/target/scala-*/soda-fountain-jetty-assembly-*.jar 2>/dev/null | tail -n 1)"
+JARFILE="soda-fountain-jetty/target/soda-fountain-jetty-assembly.jar"
 SRC_PATHS=($(find .  -maxdepth 2 -name 'src' -o -name '*.sbt' -o -name '*.scala'))
-if [ -z "$JARFILE" ] || find "${SRC_PATHS[@]}" -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
+if [ ! -f "$JARFILE" ] || find "${SRC_PATHS[@]}" -newer "$JARFILE" | egrep -q -v '(/target/)|(/bin/)'; then
     if [ "$1" == '--fast' ]; then
         echo 'Assembly is out of date.  Fast start is enabled so skipping rebuild anyway...' >&2
     else
         nice -n 19 sbt assembly >&2
-        JARFILE="$(ls -rt soda-fountain-jetty/target/scala-*/soda-fountain-jetty-assembly-*.jar 2>/dev/null | tail -n 1)"
         touch "$JARFILE"
     fi
 fi
--- bin/build_docker.sh
+++ bin/build_docker.sh
@@ -2,6 +2,6 @@
 set -e
 
 sbt soda-fountain-jetty/assembly
-jarfile="$(ls -t soda-fountain-jetty/target/scala-2.10/soda-fountain-jetty-assembly-*.jar | head -1)"
+jarfile="soda-fountain-jetty/target/soda-fountain-jetty-assembly.jar"
 cp "$jarfile" docker/soda-fountain-jetty-assembly.jar
 docker build -t soda-fountain docker/
