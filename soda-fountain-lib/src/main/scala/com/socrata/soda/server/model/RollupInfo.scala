4edc149bae63e599ddbb4bbce1280584b072bf0a
Robert J. Macomber <robert.macomber@socrata.com>
2024-02-12 15:39:41 -0800

    New-analyzer rollups are not invalid

--- soda-fountain-lib/src/main/scala/com/socrata/soda/server/model/RollupInfo.scala
+++ soda-fountain-lib/src/main/scala/com/socrata/soda/server/model/RollupInfo.scala
@@ -2,10 +2,13 @@ package com.socrata.soda.server.model
 
 import com.rojoma.json.v3.util.AutomaticJsonCodecBuilder
 import com.socrata.soda.server.id.{CopyId, RollupMapId, RollupName}
+import com.socrata.soda.server.highlevel.RollupHelper
 
 import java.time.OffsetDateTime
 
-case class RollupInfo(id: RollupMapId,copyId: CopyId,name: RollupName, soql: String, lastAccessed:OffsetDateTime)
+case class RollupInfo(id: RollupMapId,copyId: CopyId,name: RollupName, soql: String, lastAccessed:OffsetDateTime) {
+  def isNewAnalyzer = RollupHelper.isNewAnalyzerSoql(soql)
+}
 
 object RollupInfo {
   import com.rojoma.json.v3.util.time.ISO8601.codec.offsetDateTimeCodec
